c6e021fb512607923dd517b0c7c7fa5ffcc546ab
Guido Schmidt <git@guidoschmidt.cc>
2024-06-10 10:17:28 +0200

    EDIT/.gitignore + build.zig: adjust for zig 0.13.0

--- build.zig
+++ build.zig
@@ -5,12 +5,12 @@ pub fn build(b: *std.Build) !void {
     const embed = b.option([]const []const u8, "embed", "Files to embed in the binary") orelse &.{};
 
     const root = b.addModule("tokamak", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     try embedFiles(b, root, @alignCast(embed));
 
-    const tests = b.addTest(.{ .root_source_file = .{ .path = "src/main.zig" } });
+    const tests = b.addTest(.{ .root_source_file = b.path("src/main.zig") });
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_tests.step);
